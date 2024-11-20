5c1f62f344b9de7ab9f8f81dfd3b373446ba3098
Moon Ji Hoon <variableruby@gmail.com>
2024-11-21 05:07:21 +0800

    Update Project Page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,12 +1,45 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import styles from "./Projects.module.css";
+import ImageSizeDiv from "../../components/imageSizeDiv/ImageSizeDiv";
 
 function Projects() {
   return (
     <div className={styles.projects}>
-      <h1>Projects</h1>
+      <ProjectCard
+        date="13 December 2024"
+        episode="Episode.03"
+        title="ARCHIVE"
+        description="The Knowledge Base"
+      />
     </div>
   );
 }
 
 export default Projects;
+
+interface ProjectCardProps {
+  date: string;
+  episode: string;
+  title: string;
+  description: string;
+}
+
+function ProjectCard({ date, episode, title, description }: ProjectCardProps) {
+  return (
+    <div className={styles.card}>
+      <ImageSizeDiv>
+        <div className={styles.cardContent}>
+          <div className={styles.date}>{date}</div>
+          <div className={styles.cardTitleBox}>
+            <h3>{episode}</h3>
+            <h1>{title}</h1>
+          </div>
+          <div className={styles.cardDescription}>
+            <p>{description}</p>
+          </div>
+        </div>
+        <div className={styles.cardBackground} />
+      </ImageSizeDiv>
+    </div>
+  );
+}
